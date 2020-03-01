817c7c63b702f347558bf97295fb9fff0f63aa1f
Nitesh Chaudhary <niteshchaudhary@Niteshs-MacBook-Air.local>
2020-03-01 22:29:30 +0530

    Updates

---
+++ precache-manifest.8d85cf45e5f9b96784f38da5d88cfb70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f686a46420da197918e1346c66cc8d51",
+    "url": "/commenting_system/index.html"
+  },
+  {
+    "revision": "08ca93eb3a6c1fece559",
+    "url": "/commenting_system/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cd7ed6972a3b9acc0f6e",
+    "url": "/commenting_system/static/css/main.b82dfa50.chunk.css"
+  },
+  {
+    "revision": "08ca93eb3a6c1fece559",
+    "url": "/commenting_system/static/js/2.c4f28ac4.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/commenting_system/static/js/2.c4f28ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd7ed6972a3b9acc0f6e",
+    "url": "/commenting_system/static/js/main.931206cb.chunk.js"
+  },
+  {
+    "revision": "ea18c0d6751e3836ad55",
+    "url": "/commenting_system/static/js/runtime-main.f1693ee5.js"
+  }
+]);
